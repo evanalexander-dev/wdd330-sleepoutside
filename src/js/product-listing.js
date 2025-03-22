d97f3a6c53ff2e4523c8aecc87da0973df96fbcf
o-j2315 <J0sue.Hernande2@hotmail.com>
2025-03-21 23:47:01 -0600

    Refactor project structure and add product listing page

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,10 +1,5 @@
-import loadAlerts from "./alert.mjs";
-import { loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
-
-loadHeaderFooter();
-loadAlerts();
 
 const dataSource = new ProductData("tents");
 const element = document.querySelector(".product-list");
