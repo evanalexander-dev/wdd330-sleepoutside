f170d0b74bd58a72e5db1fff97e72ad399a62415
o-j2315 <J0sue.Hernande2@hotmail.com>
2025-03-22 10:23:52 -0600

    Update cart item image source to use primary small image

--- src/js/cart.js
+++ src/js/cart.js
@@ -32,7 +32,7 @@ function cartItemTemplate(item) {
   <button class="close-btn" data-id="${item.Id}">X</button>
   <a href="#" class="cart-card__image">
     <img
-      src="${item.Image}"
+      src="${item.Images.PrimarySmall}"
       alt="${item.Name}"
     />
   </a>
