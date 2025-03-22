e0efbd6b208d617d6e3608cfd565f91e8ab490f3
NicolasV99 <legolasnicolas99@gmail.com>
2025-03-22 08:58:26 -0500

    Product List Sort option

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -16,12 +16,43 @@ export default class ProductList {
       this.category = category;
       this.dataSource = dataSource;
       this.listElement = listElement;
+      this.fullList = [];
+      this.originalList = [];
     }
     async init() {
       const list = await this.dataSource.getData();
-      this.renderList(list);
+      this.fullList = list;
+      this.originalList = [...list];
+      this.renderList(this.fullList);
+      this.addSortListener();
     }
     renderList(list) {
-        renderListWithTemplate(productCardTemplate, this.listElement, list);
+      renderListWithTemplate(productCardTemplate, this.listElement, list, "afterbegin", true);
+    }
+
+    //Sort part
+    sortProducts(type) {
+      const sorted = [...this.fullList];
+      switch (type) {
+        case "name":
+          return sorted.sort((a, b) => a.NameWithoutBrand.localeCompare(b.NameWithoutBrand));
+        case "price-low-high":
+          return sorted.sort((a, b) => a.FinalPrice - b.FinalPrice);
+        case "price-high-low":
+          return sorted.sort((a, b) => b.FinalPrice - a.FinalPrice);
+        default:
+          return [...this.originalList]; 
+      }
+    }
+
+    addSortListener() {
+      const sortSelect = document.getElementById("sort");
+      if (sortSelect) {
+        sortSelect.addEventListener("change", (e) => {
+          const sortedList = this.sortProducts(e.target.value);
+          this.renderList(sortedList);
+        });
+      }
     }
 }
+
