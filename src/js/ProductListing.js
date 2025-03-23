183a4de3bb25baa37e98b6b90ef5e2a36ab33a12
laurafolk <laurafolk@yahoo.com>
2025-03-23 14:51:08 -0500

    lmf-week3-expandedInventoryCard

---
+++ src/js/ProductListing.js
@@ -0,0 +1,12 @@
+// import loadAlerts from "./alert.mjs";
+// import { loadHeaderFooter } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+
+import loadAlerts from "./alert.mjs";
+
+
+const dataSource = new ProductData("tents");
+const element = document.querySelector(".product-list");
+const productList = new ProductList("Tents", dataSource, element);
+productList.init();
