7e1178013aae165b52557ff2e244d6c10b4532c4
NicolasV99 <legolasnicolas99@gmail.com>
2025-03-26 22:37:56 -0500

    fix sort option and add it to ProductList view

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -15,16 +15,51 @@ function productCardTemplate(product) {
 }
 
 export default class ProductList {
-    constructor(category, dataSource, listElement) {
-      this.category = category;
-      this.dataSource = dataSource;
-      this.listElement = listElement;
+  constructor(category, dataSource, listElement) {
+    this.category = category;
+    this.dataSource = dataSource;
+    this.listElement = listElement;
+    this.fullList = [];
+    this.originalList = [];
+  }
+
+  async init() {
+    try {
+      const list = await this.dataSource.getData(this.category);
+      this.fullList = list;
+      this.originalList = [...list];
+      this.renderList(this.fullList);
+      this.addSortListener();
+    } catch (err) {
+      this.listElement.innerHTML = "<p>Unable to load products at this time. Please try again later.</p>";
     }
-    async init() {
-      const list = await this.dataSource.getData();
-      this.renderList(list);
+  }
+
+  renderList(list) {
+    renderListWithTemplate(productCardTemplate, this.listElement, list, "afterbegin", true);
+  }
+
+  sortList(type) {
+    const sorted = [...this.fullList];
+    switch (type) {
+      case "name":
+        return sorted.sort((a, b) => a.Name.localeCompare(b.Name));
+      case "price-low-high":
+        return sorted.sort((a, b) => a.FinalPrice - b.FinalPrice);
+      case "price-high-low":
+        return sorted.sort((a, b) => b.FinalPrice - a.FinalPrice);
+      default:
+        return [...this.originalList];
     }
-    renderList(list) {
-        renderListWithTemplate(productCardTemplate, this.listElement, list);
+  }
+
+  addSortListener() {
+    const select = document.getElementById("sort");
+    if (select) {
+      select.addEventListener("change", (e) => {
+        const sorted = this.sortList(e.target.value);
+        this.renderList(sorted);
+      });
     }
+  }
 }
