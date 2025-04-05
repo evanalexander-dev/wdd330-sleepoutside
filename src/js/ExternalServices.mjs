bd666acb330c7656b8cf8cabd542ffe2afd034b6
John Adebayo <johnayomide31@gmail.com>
2025-04-05 15:11:25 +0100

    Refactor checkout process and adding success and wishlist pages

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -1,10 +1,13 @@
 const baseURL = import.meta.env.VITE_SERVER_URL;
 
-function convertToJson(res) {
+
+async function convertToJson(res) {
+  const jsonResponse = await res.json();
+  // const
   if (res.ok) {
-    return res.json();
+    return jsonResponse;
   } else {
-    throw new Error("Bad Response");
+    throw { name: 'servicesError', message: jsonResponse };
   }
 }
 
